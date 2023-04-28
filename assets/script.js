5941bb37dca09ee8457c44386635fdb77b8a865e
DawsonBolen <dawson120@live.com>
2023-04-28 15:30:07 -0700

    made some important changes to game data

--- assets/script.js
+++ assets/script.js
@@ -44,12 +44,18 @@ var imageContainer = document.querySelector('.image-container');
 var gameStatsSection = document.querySelector('.game-stats');
 var gamesList = document.querySelector('#games-list');
 
+
 function handleSearch(event) {
   var teamTitle = document.querySelector('#teamtitle');
   var conference = document.querySelector('#conference');
   var division = document.querySelector('#division');
   var teamsUrl = 'https://www.balldontlie.io/api/v1/teams/';
-teamSearchInputVal = teamSearchInput.value;
+  gamesList.innerText = "";
+  teamNameDisplay.textContent = " ";
+  seasonDisplay.textContent = "";
+   wins = 0;
+   losses = 0;
+  teamSearchInputVal = teamSearchInput.value;
   let requestUrl = teamsUrl + teamSearchInputVal;
   if (!teamSearchInputVal) {
       return;
@@ -106,46 +112,79 @@ function getRoster() {
     .then(function (data){
         //console.log(data);
         displayScore(data);
-        gameStatsSection.classList.remove('hide');
-        for (var i = 0; i < data.length; i++) {
-            var li = document.createElement('li');
-            li.textContent = data[i].home_team_score;
-            console.log('hello');
-            console.log(data[i].home_team_score);
-            //document.body.appendChild(li);
-            gamesList.appendChild(li);
-            
-            
-        }
+        gameStatsSection.classList.remove('hide');      
+        
     })
   }
 
+
+  var recordDisplayWins = document.querySelector('#recordwins');
+  var recordDisplayLosses = document.querySelector('#recordlosses');
+  var winPercentage = document.querySelector('#winpercentage');
+  var teamNameDisplay = document.querySelector('#team-name-record');
+  var seasonDisplay = document.querySelector('#team-record-year');
+
   function displayScore(data){
-    console.log(data);
+    let teamSearchInputVal = teamSearchInput.value;
+
+   
+
+    
     for (let i = 0; i < data.data.length; i++) {
-      var li = document.createElement('li');
-      
-      var homeTeam = document.createElement('div');
-      var awayTeam = document.createElement('div');
-      homeTeam.innerText = data.data[i].home_team_score;
-      awayTeam.innerText = data.data[i].visitor_team_score;
-     
-      //li.innerText = data.data[i].home_team_score;
-      li.appendChild(homeTeam);
-      li.appendChild(awayTeam);
     
 
+      if (teamSearchInputVal == data.data[i].home_team.id) {
+        selectedTeamScore = data.data[i].home_team_score;
+        opponentScore = data.data[i].visitor_team_score;
+        selectedTeamName = data.data[i].home_team.full_name;
+        oponentName = data.data[i].visitor_team.full_name;
+    } else  {
+     
+      selectedTeamScore = data.data[i].visitor_team_score;
+      opponentScore = data.data[i].home_team_score;
+      oponentName = data.data[i].home_team.full_name;
+      selectedTeamName = data.data[i].visitor_team.full_name;
+    }
+
+
+
+      var li = document.createElement('li');
+      var homeTeam = document.createElement('div');
+      var awayTeam = document.createElement('div');
+      var awayTeamName = document.createElement('div');
+      homeTeam.innerText = selectedTeamScore;
+      awayTeam.innerText = opponentScore;
+      awayTeamName.innerText = "vs" + " " + oponentName;
+      //li.innerText = data.data[i].home_team_score;
+      li.appendChild(homeTeam);
+      li.appendChild(awayTeamName);
+      li.appendChild(awayTeam);
+     // console.log(data);
+    
+     // let selectedTeamScore;
+      //let opponentScore;
+      //home team isnt always the selected team so it requires another conditional to determine that
+    
+     
+     if (selectedTeamScore > opponentScore) {
+      li.setAttribute("style", "background-color: #2bd670;");
+      wins = wins +1;
+    } else {
+      li.setAttribute("style", "background-color: #cc3b3b;");
+      losses = losses+1;
+    }
+    
+      let winPercentageVal = wins / 82;
+      recordDisplayWins.textContent = "W" + " " + wins;
+      recordDisplayLosses.textContent = "L" + " " + losses;
+      winPercentage.textContent = "W-L:" + " " + Math.round((winPercentageVal + Number.EPSILON) * 100)  + "%";
+      teamNameDisplay.textContent = selectedTeamName + " ";
+      seasonDisplay.textContent = chosenSeason + " " + "Season:";
       //document.body.appendChild(li);
       gamesList.appendChild(li);
       
-      if (data.data[i].home_team_score > data.data[i].visitor_team_score) {
-        li.setAttribute("style", "background-color: #2BD670;");
-      } else {
-        li.setAttribute("style", "background-color: #CC3B3B;");
-      }
       
   }
   }
 
   submitButton.addEventListener("click", handleSearch);
-  
